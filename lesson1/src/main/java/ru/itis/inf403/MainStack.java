28b127e23e7b9811bf6133e59b7af9ee8d320fb0
ildarsafa5 <ildarsafa5@gmail.com>
2025-04-28 07:58:20 +0300

    добавил пару тестов

--- lesson1/src/main/java/ru/itis/inf403/MainStack.java
+++ lesson1/src/main/java/ru/itis/inf403/MainStack.java
@@ -5,7 +5,13 @@ import java.util.Stack;
 public class MainStack {
     public static void main(String[] args) {
         String s1 = "({[}])";
+        String s2 = "((((())";
+        String s3 = "(()[{}])";
+        String s4 = "(())";
         System.out.println(check(s1));
+        System.out.println(check(s2));
+        System.out.println(check(s3));
+        System.out.println(check(s4));
     }
 
     public static boolean check(String str) {
